9e67c2019a8f6917c9673b5a12b1f12afc05d19a
Guilherme Coelho <guicoelho0411@gmail.com>
2025-09-18 22:42:39 -0300

    Adiciona rota de teste de log para o Render

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,12 @@ const bookingRoutes = require('./routes/bookings');
 const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = 3001;
+// --- ROTA DE TESTE PARA LOGS ---
+app.get('/test-render', (req, res) => {
+  console.log('--- ✅✅✅ O LOG NO RENDER ESTÁ FUNCIONANDO! ✅✅✅ ---');
+  res.send('Teste de log do servidor online recebido com sucesso!');
+});
+// 
 app.use(cors());
 app.use(express.json());
 app.use('/api/bookings', bookingRoutes);
